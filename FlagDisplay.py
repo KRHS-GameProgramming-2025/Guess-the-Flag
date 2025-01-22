f32513639bcd32b8a7b055563b6e4c9ee5258fe4
Ethan Seal <Ethan.Seal@kearsarge.org>
2025-01-22 12:44:33 -0500

    Added States

--- FlagDisplay.py
+++ FlagDisplay.py
@@ -1,7 +1,9 @@
 import pygame, sys, math
 
 class StateFlag():
-    def __init__(self,name,startpos=[0,0], size=[1024, 900]):
+    def __init__(self,name,startpos=[0,0], scale=1):
         self.image = pygame.image.load("Art/StateFlags/Flag_of_"+ name+".png")
-        self.image=pygame.transform.scale(self.image,size)
-        self.rect = self.image.get_rect(center=startpos)
+        self.rect = self.image.get_rect(midtop=startpos)
+        
+        self.image=pygame.transform.scale(self.image,[self.rect.width*scale,self.rect.height*scale])
+        self.rect = self.image.get_rect(midtop=startpos)
