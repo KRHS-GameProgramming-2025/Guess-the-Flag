c24188e3f54e00395db5f2304068e99945cf1699
Liam Martlew <liammartlew100@gmail.com>
2025-02-12 12:18:29 -0500

    european flags have all been resized flags are now loaded from directory instead of list rizz rizz rizz rizz rizz

--- FlagDisplay.py
+++ FlagDisplay.py
@@ -1,9 +1,10 @@
 import pygame, sys, math
 
-class StateFlag():
-    def __init__(self,name,startpos=[0,0], scale=1):
-        self.image = pygame.image.load("Art/StateFlags/Flag_of_"+ name+".png")
+class Flag():
+    def __init__(self,path, name,startpos=[0,0], scale=1):
+        self.image = pygame.image.load(path+"/Flag_of_"+ name+".png")
         self.rect = self.image.get_rect(midtop=startpos)
+        self.name = name.replace("_", " ")
         
         self.image=pygame.transform.scale(self.image,[self.rect.width*scale,self.rect.height*scale])
         self.rect = self.image.get_rect(midtop=startpos)
--- FlagGame.py
+++ FlagGame.py
@@ -1,4 +1,4 @@
-import pygame, sys, math, random
+import pygame, sys, math, random, os
 from FlagDisplay import*
 from Button import*
 pygame.init()
@@ -7,65 +7,30 @@ clock = pygame.time.Clock();
 size = [968, 1000]
 screen = pygame.display.set_mode(size)
 
-states = ["Alabama",
-          "Alaska",
-          "Arizona",
-          "Arkansas",
-          "California",
-          "Colorado",
-          "Connecticut",
-          "Delaware",
-          "Florida",
-          "Georgia",
-          "Hawaii",
-          "Idaho",
-          "Illinois",
-          "Indiana",
-          "Iowa",
-          "Kansas",
-          "Kentucky",
-          "Louisiana",
-          "Maine",
-          "Maryland",
-          "Massachusetts",
-          "Michigan",
-          "Minnesota",
-          "Mississippi",
-          "Missouri",
-          "Montana",
-          "Nebraska",
-          "Nevada",
-          "New_Hampshire",
-          "New_Jersey",
-          "New_Mexico",
-          "New_York",
-          "North_Carolina",
-          "North_Dakota",
-          "Ohio",
-          "Oklahoma",
-          "Oregon",
-          "Pennsylvania",
-          "Rhode_Island",
-          "South_Carolina",
-          "South_Dakota",
-          "Tennessee",
-          "Texas",
-          "Utah",
-          "Vermont",
-          "Virginia",
-          "Washington",
-          "West_Virginia",
-          "Wisconsin",
-          "Wyoming"]
-                                                                                         
+def getFlags(kind):
+    if kind == "State Flags":
+        path = "Art/StateFlags"
+    elif kind == "Europe Flags":
+        path = "Art/EuropeFlags"
+    files = os.listdir(path)
+    
+    flags = []
+    for f in files:
+        if f[-4:] == ".png":
+            flags += [Flag(path, f[8:-4], [size[0]/2,50], .75)]
+    return flags
+    
+
+flags = getFlags("Europe Flags")                                                                                
+                                                                                
           
 
 s=0
-flag = StateFlag(states[s], [size[0]/2,50],.75)
-button1=Button(states[s], [size[0]/4,750],.5)
-button2=Button(states[s], [size[0]/4,850],.5)
-button3=Button(states[s], [size[0]/4*3,750],.5)
-button4=Button(states[s], [size[0]/4*3,850],.5)
+flag = flags[s]
+button1=Button(flags[s].name, [size[0]/4,750],.5)
+button2=Button(flags[s].name, [size[0]/4,850],.5)
+button3=Button(flags[s].name, [size[0]/4*3,750],.5)
+button4=Button(flags[s].name, [size[0]/4*3,850],.5)
 
 
 while True:
@@ -76,23 +41,23 @@ while True:
             if event.key == pygame.K_RIGHT:
                 s+=1
                 try:
-                    flag = StateFlag(states[s], [size[0]/2,50],.75)
-                    button1=Button(states[s], [size[0]/4,750],.5)
-                    button2=Button(states[s], [size[0]/4,850],.5)
-                    button3=Button(states[s], [size[0]/4*3,750],.5)
-                    button4=Button(states[s], [size[0]/4*3,850],.5)
+                    flag = flags[s] 
+                    button1=Button(flags[s].name, [size[0]/4,750],.5)
+                    button2=Button(flags[s].name, [size[0]/4,850],.5)
+                    button3=Button(flags[s].name, [size[0]/4*3,750],.5)
+                    button4=Button(flags[s].name, [size[0]/4*3,850],.5)
                 except:
-                    s-=1
+                    s=0
             if event.key == pygame.K_LEFT:
                 s-=1
                 try:
-                    flag = StateFlag(states[s], [size[0]/2,50],.75)
-                    button1=Button(states[s], [size[0]/4,750],.5)
-                    button2=Button(states[s], [size[0]/4,850],.5)
-                    button3=Button(states[s], [size[0]/4*3,750],.5)
-                    button4=Button(states[s], [size[0]/4*3,850],.5)
+                    flag = flags[s]
+                    button1=Button(flags[s].name, [size[0]/4,750],.5)
+                    button2=Button(flags[s].name, [size[0]/4,850],.5)
+                    button3=Button(flags[s].name, [size[0]/4*3,750],.5)
+                    button4=Button(flags[s].name, [size[0]/4*3,850],.5)
                 except:
-                    s+=1
+                    s= len(flags)-1
             
             
     
