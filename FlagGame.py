486d954e378d033c42998238b7806a68fb04f506
Ethan Seal <Ethan.Seal@kearsarge.org>
2025-03-05 12:04:06 -0500

    remved oregon revesrve added random flag pattern and buttons

--- FlagGame.py
+++ FlagGame.py
@@ -19,18 +19,37 @@ def getFlags(kind):
         if f[-4:] == ".png":
             flags += [Flag(path, f[8:-4], [size[0]/2,50], .75)]
     return flags
-    
 
-flags = getFlags("Europe Flags")                                                                                
-                                                                                
+def buildButtons(answer,flaglist):
+    correct = random.randint(0,3)
+    locations = [[size[0]/4,750],
+                 [size[0]/4,850],
+                 [size[0]/4*3,750],
+                 [size[0]/4*3,850]]
+    buttons = []
+    buttonTexts = [answer]
+    for i, button in enumerate(range(4)):
+        if i == correct:
+            buttons += [Button(answer, locations[i],.5)]
+        else:
+            text = answer
+            while text in buttonTexts:
+                num = random.randint(0, len(flaglist))
+                text = flaglist[num].name
+            buttonTexts += [text]
+            buttons += [Button(text, locations[i],.5)]
+    return buttons
+
+flags = getFlags("State Flags")                                                                                
+random.shuffle(flags)
+flagNames = []
+for flag in flags:
+    flagNames += [flag.name]                                                                              
           
 
 s=0
 flag = flags[s]
-button1=Button(flags[s].name, [size[0]/4,750],.5)
-button2=Button(flags[s].name, [size[0]/4,850],.5)
-button3=Button(flags[s].name, [size[0]/4*3,750],.5)
-button4=Button(flags[s].name, [size[0]/4*3,850],.5)
+buttons=buildButtons(flag.name, flags)
 
 
 while True:
@@ -42,20 +61,14 @@ while True:
                 s+=1
                 try:
                     flag = flags[s] 
-                    button1=Button(flags[s].name, [size[0]/4,750],.5)
-                    button2=Button(flags[s].name, [size[0]/4,850],.5)
-                    button3=Button(flags[s].name, [size[0]/4*3,750],.5)
-                    button4=Button(flags[s].name, [size[0]/4*3,850],.5)
+                    buttons=buildButtons(flag.name, flags)
                 except:
                     s=0
             if event.key == pygame.K_LEFT:
                 s-=1
                 try:
                     flag = flags[s]
-                    button1=Button(flags[s].name, [size[0]/4,750],.5)
-                    button2=Button(flags[s].name, [size[0]/4,850],.5)
-                    button3=Button(flags[s].name, [size[0]/4*3,750],.5)
-                    button4=Button(flags[s].name, [size[0]/4*3,850],.5)
+                    buttons=buildButtons(flag.name, flags)
                 except:
                     s= len(flags)-1
             
@@ -64,9 +77,7 @@ while True:
     
     screen.fill((181, 181, 181))  
     screen.blit(flag.image,flag.rect)
-    screen.blit(button1.image,button1.rect)
-    screen.blit(button2.image,button2.rect)
-    screen.blit(button3.image,button3.rect)
-    screen.blit(button4.image,button4.rect)
+    for button in buttons:
+        screen.blit(button.image, button.rect)
     pygame.display.flip()
     clock.tick(60)
