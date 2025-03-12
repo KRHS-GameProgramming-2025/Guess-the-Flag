a32f1913585dd5bb0dc78844d4b4f508f0b98ef6
Max Grey <Maximus.grey@kearsarge.org>
2025-03-12 12:20:19 -0400

    Start Screen in

--- FlagGame.py
+++ FlagGame.py
@@ -14,6 +14,7 @@ lose=pygame.mixer.Sound("Sound/Booooooooooo.mp3")
 win=pygame.mixer.Sound("Sound/Winning Game.mp3")
 
 winThreshold = .80
+clicked = False
 
 def getFlags(kind):
     if kind == "State Flags":
@@ -48,84 +49,146 @@ def buildButtons(answer,flaglist):
             buttons += [Button(text, locations[i],.5)]
     return buttons
 
-flags = getFlags("State Flags")                                                                                
-random.shuffle(flags)
-flagNames = []
-for flag in flags:
-    flagNames += [flag.name]                                                                              
-          
 
-s=0
-flag = flags[s]
-buttons=buildButtons(flag.name, flags)
-clicked = False
-points = 0
-score = Hud("Score: ", points, "right", [0,0])
-progress = Hud ("/"+str(len(flags)), s, "left", [968,0])
+mode="Start"
+Version=""
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit();
-        # ~ if event.type == pygame.KEYDOWN:
-            # ~ if event.key == pygame.K_RIGHT:
-                # ~ s+=1
-                # ~ try:
-                    # ~ flag = flags[s] 
-                    # ~ buttons=buildButtons(flag.name, flags)
-                # ~ except:
-                    # ~ s=0
-            # ~ if event.key == pygame.K_LEFT:
-                # ~ s-=1
-                # ~ try:
-                    # ~ flag = flags[s]
-                    # ~ buttons=buildButtons(flag.name, flags)
-                # ~ except:
-                    # ~ s= len(flags)-1
-        if event.type == pygame.MOUSEMOTION:
-            for button in buttons:
-                button.collidePoint(event.pos, clicked)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                clicked = True
+    bgimage=pygame.image.load("Art/Screens/flagger.png")
+    bgrect=bgimage.get_rect()
+    buttons = [Button("State Flags",[968/2,200], .5),
+               Button("Europe Flags",[968/2,300], .5)]
+    
+    while mode =="Start":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit();
+            if event.type == pygame.MOUSEMOTION:
                 for button in buttons:
                     button.collidePoint(event.pos, clicked)
-        if event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                clicked = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    clicked = True
+                    for button in buttons:
+                        button.collidePoint(event.pos, clicked)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    clicked = False
+                    for button in buttons:
+                        if button.collidePoint(event.pos, clicked):
+                            Version = button.name
+                            mode = "Play"
+        
+        screen.fill((181, 181, 181))  
+        screen.blit(bgimage,bgrect)
+        for button in buttons:
+            screen.blit(button.image, button.rect)
+        pygame.display.flip()
+        clock.tick(60)
+        
+    flags = getFlags(Version)                                                                                
+    random.shuffle(flags)
+    flagNames = []
+    for flag in flags:
+        flagNames += [flag.name]                                                                              
+              
+
+    s=0
+    flag = flags[s]
+    buttons=buildButtons(flag.name, flags)
+    
+    points = 0
+    score = Hud("Score: ", points, "right", [0,0])
+    progress = Hud ("/"+str(len(flags)), s, "left", [968,0])
+
+    while mode =="Play":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit();
+            # ~ if event.type == pygame.KEYDOWN:
+                # ~ if event.key == pygame.K_RIGHT:
+                    # ~ s+=1
+                    # ~ try:
+                        # ~ flag = flags[s] 
+                        # ~ buttons=buildButtons(flag.name, flags)
+                    # ~ except:
+                        # ~ s=0
+                # ~ if event.key == pygame.K_LEFT:
+                    # ~ s-=1
+                    # ~ try:
+                        # ~ flag = flags[s]
+                        # ~ buttons=buildButtons(flag.name, flags)
+                    # ~ except:
+                        # ~ s= len(flags)-1
+            if event.type == pygame.MOUSEMOTION:
                 for button in buttons:
-                    if button.collidePoint(event.pos, clicked):
-                        if button.name == flag.name:
-                            points += 1
-                            score.update(points)
-                            correct.play()
-                        else:
-                            wrong.play()
-                        s+=1
-                        progress.update(s)
-                        try:
-                            flag = flags[s] 
-                            buttons=buildButtons(flag.name, flags)
-                        except:
-                            if points/len(flags) > winThreshold:
-                                print("Winner")
-                                win.play()
+                    button.collidePoint(event.pos, clicked)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    clicked = True
+                    for button in buttons:
+                        button.collidePoint(event.pos, clicked)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    clicked = False
+                    for button in buttons:
+                        if button.collidePoint(event.pos, clicked):
+                            if button.name == flag.name:
+                                points += 1
+                                score.update(points)
+                                correct.play()
                             else:
-                                lose.play()
-                                print("lose")
-                            
-                
-            
-            
-            
-            
+                                wrong.play()
+                            s+=1
+                            progress.update(s)
+                            try:
+                                flag = flags[s] 
+                                buttons=buildButtons(flag.name, flags)
+                            except:
+                                if points/len(flags) > winThreshold:
+                                    print("Winner")
+                                    win.play()
+                                    mode = "win"
+                                else:
+                                    lose.play()
+                                    print("lose")
+                                    mode = "lose"
+
+        screen.fill((181, 181, 181))  
+        screen.blit(flag.image,flag.rect)
+        for button in buttons:
+            screen.blit(button.image, button.rect)
+        screen.blit(score.image, score.rect)
+        screen.blit(progress.image, progress.rect)
+        pygame.display.flip()
+        clock.tick(60)
+        
+    bgimage=pygame.image.load("Art/Screens/YOU WIN.png")
+    bgrect=bgimage.get_rect()
+    buttons = [Button("Play Again",[968/2,200], .5)]
     
-    
-    screen.fill((181, 181, 181))  
-    screen.blit(flag.image,flag.rect)
-    for button in buttons:
-        screen.blit(button.image, button.rect)
-    screen.blit(score.image, score.rect)
-    screen.blit(progress.image, progress.rect)
-    pygame.display.flip()
-    clock.tick(60)
+    while mode =="win":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit();
+            if event.type == pygame.MOUSEMOTION:
+                for button in buttons:
+                    button.collidePoint(event.pos, clicked)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    clicked = True
+                    for button in buttons:
+                        button.collidePoint(event.pos, clicked)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    clicked = False
+                    for button in buttons:
+                        if button.collidePoint(event.pos, clicked):
+                            mode = "start"
+        
+        screen.fill((181, 181, 181))  
+        screen.blit(bgimage,bgrect)
+        for button in buttons:
+            screen.blit(button.image, button.rect)
+        pygame.display.flip()
+        clock.tick(60)
