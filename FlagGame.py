f2117f5d6cf7c9f4983b980b06b23b5b2bde9f06
Liam Martlew <liammartlew100@gmail.com>
2025-04-14 12:11:28 -0400

    Revert "h"
    This reverts commit 42779f99a49f93372b72d12baf229dabaa00c3e1.

--- FlagGame.py
+++ FlagGame.py
@@ -61,6 +61,7 @@ while True:
                Button("Asia Flags",[968/4*3, 200], .5),
                Button("Africa Flags", [968/4*3,300], .5),
                Button("Oceania Flags",[968/4*3,400], .5),
+               Button("WWI Flags",[968/4*3,500], .5),
                
                
                ]
@@ -102,6 +103,7 @@ while True:
     s=0
     flag = flags[s]
     buttons=buildButtons(flag.name, flags)
+    buttons+= [Button("Back", [size[0]/6, 950], .5)]
     
     points = 0
     score = Hud("Score: ", points, "right", [0,0])
@@ -143,6 +145,9 @@ while True:
                                 points += 1
                                 score.update(points)
                                 correct.play()
+                            elif button.name == "Back":
+                                mode="Start"
+                                break
                             else:
                                 wrong.play()
                             s+=1
@@ -150,6 +155,7 @@ while True:
                             try:
                                 flag = flags[s] 
                                 buttons=buildButtons(flag.name, flags)
+                                buttons+= [Button("Back", [size[0]/6, 950], .5)]
                             except:
                                 if points/len(flags) > winThreshold:
                                     print("Winner")
